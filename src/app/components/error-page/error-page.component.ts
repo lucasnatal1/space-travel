1e8a7838f6973267bb81069642efe1142a1df5c0
lucasnatal1 <62300374+lucasnatal1@users.noreply.github.com>
2023-06-29 21:34:40 -0300

    Animation following mouse

--- src/app/components/error-page/error-page.component.ts
+++ src/app/components/error-page/error-page.component.ts
@@ -3,12 +3,41 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-error-page',
   templateUrl: './error-page.component.html',
-  styleUrls: ['./error-page.component.css']
+  styleUrls: ['./error-page.component.css'],
 })
 export class ErrorPageComponent {
-  stars = [...Array(80).keys()]
+  stars = [...Array(80).keys()];
 
-  constructor() {
-    console.log(this.stars);
+  onMouseMove(e: any) {
+    const planet = document.getElementById('planet');
+
+    let domainX = [0, document.body.clientWidth],
+        domainY = [0, document.body.clientHeight],
+        range = [-10, 10];
+
+    let translate = {
+      x:
+        range[0] +
+        ((e.clientX - domainX[0]) * (range[1] - range[0])) /
+          (domainX[1] - domainX[0]),
+      y:
+        range[0] +
+        ((e.clientY - domainY[0]) * (range[1] - range[0])) /
+          (domainY[1] - domainY[0]),
+    };
+
+    if(planet) {
+      planet.style.animation = 'none';
+      planet.style.transform = `translate(${translate.x}px, ${translate.y}px)`;
+    }
+    
+  }
+
+  onMouseLeave() {
+    console.log("MOUSELEAVE");
+    const planet = document.getElementById('planet');
+    if(planet) {
+      planet.style.animation = '';
+    }
   }
 }
